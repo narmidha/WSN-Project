8985e57b9205d12b0f768916307c8708ac7f1ad4
narmidha <132040574+narmidha@users.noreply.github.com>
2024-11-20 19:49:46 +0530

    Add files via upload

---
+++ AUT Graph/graph.tcl
@@ -0,0 +1,8 @@
+
+exec xgraph EXT-thr.tr   PRO-thr.tr    -bg white -geometry 800*500 &
+
+exec xgraph EXT-energy.tr PRO-energy.tr -bg white -geometry 800*500 &
+
+exec xgraph EXT-delay.tr PRO-delay.tr   -bg white -geometry 800*500 &
+
+
